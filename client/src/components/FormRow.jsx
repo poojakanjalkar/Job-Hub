f83fb7da3efbdb9f989d435574244b0ff5364a28
Pooja Kanjalkar <poojakanjalkar554@gmail.com>
2024-05-04 14:58:12 +0530

    added code for register page

---
+++ client/src/components/FormRow.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function FormRow({ type, labelText, name, defaultValue = "" }) {
+  return (
+    <div className='form-row'>
+      <label htmlFor={name} className='form-label'>
+        {labelText || name}
+
+      </label>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        className='form-input'
+        defaultValue={defaultValue}
+        required
+      />
+    </div>
+  )
+}
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import { Logo } from '../components';
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import { Link } from 'react-router-dom';
+import FormRow from '../components/FormRow';
 
 export default function Register() {
   return (
-    <h1>Register</h1>
+    <Wrapper>
+      <form className='form'>
+        <Logo />
+        <h4>Register</h4>
+        <FormRow type="text" name="name" />
+        <FormRow type="text" name="lastName" labelText="last name" />
+        <FormRow type="text" name="location" />
+        <FormRow type="email" name="email" />
+        <FormRow type='password' name='password' />
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+        <p>
+          Already a member?
+          <Link to='/login' className='member-btn'>
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   )
 }
