b43bd865fa55378afec65a4761b3e03e06318b3f
Pooja Kanjalkar <poojakanjalkar554@gmail.com>
2024-05-05 09:26:32 +0530

    added login page and css for register and login

--- client/src/assets/wrappers/RegisterAndLoginPage.js
+++ client/src/assets/wrappers/RegisterAndLoginPage.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Wrapper = styled.section`
   min-height: 100vh;
@@ -20,7 +20,7 @@ const Wrapper = styled.section`
   p {
     margin-top: 1rem;
     text-align: center;
-    line-height: 1.5;
+    line-height: 1.6;
   }
   .btn {
     margin-top: 1rem;
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import Logo from '../components/Logo'
+import FormRow from '../components/FormRow'
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+
+import { Link } from 'react-router-dom';
 
 export default function Login() {
   return (
-    <h1>login</h1>
+    <Wrapper>
+      <form className='form'>
+        <Logo />
+        <h4>Login</h4>
+        <FormRow type='email' name='email' defaultValue='john@gmail.com' />
+        <FormRow type='password' name='password' defaultValue='secret123' />
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+        <button type='button' className='btn btn-block'>
+          explore the app
+        </button>
+        <p>
+          Not a member yet?
+          <Link to='/register' className='member-btn'>
+            Register
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   )
 }
