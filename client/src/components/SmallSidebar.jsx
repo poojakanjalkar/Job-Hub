b44fc44032fab8f73ebf5d7f6d351bdc6323f6e9
Pooja Kanjalkar <poojakanjalkar554@gmail.com>
2024-05-14 18:35:54 +0530

    added small sidebar and link component

--- client/src/components/SmallSidebar.jsx
+++ client/src/components/SmallSidebar.jsx
@@ -1,11 +1,50 @@
 import React from 'react'
 import Wrapper from '../assets/wrappers/SmallSidebar'
+import { useDashboardContext } from '../pages/DashboardLayout';
+import { FaTimes } from 'react-icons/fa';
+import Logo from './Logo';
+import links from '../utils/links';
+import { NavLink } from 'react-router-dom';
 
 export default function SmallSidebar() {
+
+  const { showSidebar, toggleSidebar } = useDashboardContext();
   return (
     <Wrapper>
-      <div><h1>SmallSidebar</h1></div>
-    </Wrapper>
+      <div
+        className={
+          showSidebar ? "sidebar-container show-sidebar" : "sidebar-container"
+        }
+      >
+        <div className="content">
+          <button type="button" className="close-btn" onClick={toggleSidebar}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">
+            {links.map((link) => {
+              const { text, path, icon } = link;
 
-  )
+              return (
+                <NavLink
+                  to={path}
+                  key={text}
+                  className="nav-link"
+                  onClick={toggleSidebar}
+
+                  end //since we have nested structure dashboard is parrent add job is always active so to disable it use end prop
+                >
+                  <span className="icon">{icon}</span>
+                  {text}
+                </NavLink>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
+
 }
