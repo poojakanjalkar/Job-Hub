c09c890288976c5f7c16c56c1b560d5c993aaacc
Pooja Kanjalkar <poojakanjalkar554@gmail.com>
2024-05-25 18:29:22 +0530

    introduced with morgan middleware

--- API/server.js
+++ API/server.js
@@ -1,13 +1,27 @@
 //using ES6 in node
-
+import * as dotenv from "dotenv";
+dotenv.config();
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev")); //it provides info about response time and content_length
+}
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("hello world");
 });
 
-app.listen(3000, () => {
-  console.log("server running on 3000");
+app.post("/", (req, res) => {
+  const result = req.body;
+  res.json({ message: "REsul success", result });
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}...`);
 });
